edbddf5e432c7866d333d8998386f1db884a3150
Githubuser289 <moldovan.mms@gmail.com>
2023-12-11 20:03:50 +0200

    render movies-vers.1

--- src/index.js
+++ src/index.js
@@ -101,10 +101,23 @@ async function initializePage() {
   const result = await fetchTrendingMovies();
   processMoviesData(result);
 
-  // renderMoviesList(movieArray);
+  renderMoviesList(movieArray);
 
   // **************
   console.log('1 movieArray= ', movieArray);
 }
-
+function renderMoviesList(list) {
+  const markup = list
+    .map(({ poster_path, title, genres, release_year, vote_average }) => {
+      // adaug elemente in markup
+      return `<div>
+    <img src="${poster_path}" alt="movie poster" loading="lazy" />
+            <p>   ${title} </p>
+            <p> ${genres} ${release_year} ${vote_average} </p>
+        </div>`;
+    })
+    .join('');
+  const bodyElem = document.querySelector('body');
+  bodyElem.insertAdjacentHTML('beforeend', markup);
+}
 initializePage();
